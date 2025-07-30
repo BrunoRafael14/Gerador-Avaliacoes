4dcbfa629b90bb5d3b6243e5e4ec6439d1f2d2ed
BrunoRafael14 <brunorafaelfilho10@gmail.com>
2025-07-30 20:47:27 -0300

    Adicionando menu básico

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import json
 import os
+
+print("Selecione uma opção: ")
+print(" 1 - Calcular PTAM \n 2 - Calcular Desconto \n 3 - Adicionar valor de M² \n 4 - Sair")
+
+menu = int(input(""))
+
+if menu == 1:
+    print("1")
+elif menu == 2:
+    print("2")
+elif menu == 3:
+    print("3")
+else:
+    print("Saindo do programa.")
+    exit()
