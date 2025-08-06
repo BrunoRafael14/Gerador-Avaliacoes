430873aa7c15349a96d637ce236f9d0371631ebf
BrunoRafael14 <brunorafaelfilho10@gmail.com>
2025-08-05 23:42:15 -0300

    Melhorando o sistema de desconto

--- funcoes/funcoes_calculos.py
+++ funcoes/funcoes_calculos.py
@@ -1,6 +1,9 @@
-def calcular_taxas(area, valor_m2):
-    valor_terreno = area * valor_m2
+def calcular_taxas(valor_terreno):
     valor_foro = valor_terreno * 0.003
     valor_laudemio = valor_terreno * 0.025  
     valor_dominio = valor_terreno * 0.12   
-    return valor_terreno, valor_foro, valor_laudemio, valor_dominio
+    return valor_foro, valor_laudemio, valor_dominio
+
+def calculo_desconto_personalizado(valor_taxa, desconto_personalizado):
+    desconto = valor_taxa - (valor_taxa * (desconto_personalizado / 100))
+    return desconto
