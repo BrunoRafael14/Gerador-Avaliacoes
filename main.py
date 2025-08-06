6ef98293a94e3a940ff6c4f46845255ca2b39239
BrunoRafael14 <brunorafaelfilho10@gmail.com>
2025-08-05 23:51:30 -0300

    f

--- main.py
+++ main.py
@@ -57,49 +57,51 @@ while True:
             valor_foro, valor_laudemio, valor_dominio = funcoes.funcoes_calculos.calcular_taxas(valor_terreno)
 
             print("Digite Tipo de desconto que deseja aplicar: \n1 - Pré-definidos \n2 - Personalizado")
-            tipo_desconto = int(input(""))
-            if tipo_desconto == 1:
-                
-                print("Escolha as taxas para desconto: \n1 - Apenas foro \n2 - Foro e Laudêmio \n3 - Foro, Laudemio e Dominio \n4 - Foro e Dominio")
-                taxas_escolhidas = int(input(""))
-                if taxas_escolhidas == 1:
-                    print("----------------------------------------------------")
-                    print(f"Desconto de 5% sobre o Foro: R$ {(valor_foro * quantidade_foros) - ((valor_foro * quantidade_foros) * 0.05):.2f}")
-                    print("----------------------------------------------------")
-                    teste = input("Gostaria de calcular outro desconto? (s/n): ").strip().lower()
-                    if teste == "sim" or teste == "s":
-                        continue
-                    else:
-                        break
-                elif taxas_escolhidas == 2:
-                    print("----------------------------------------------------")
-                    print(f"Desconto de 8% sobre o Foro e Laudêmio: R$ {((valor_foro * quantidade_foros) + valor_laudemio) - (((valor_foro * quantidade_foros) + valor_laudemio) * 0.08):.2f} \nDesconto de 10% sobre o Foro e Laudêmio: R$ {((valor_foro * quantidade_foros) + valor_laudemio) - (((valor_foro * quantidade_foros) + valor_laudemio) * 0.10):.2f}")
-                    print("----------------------------------------------------")
-                    teste = input("Gostaria de calcular outro desconto? (s/n): ").strip().lower()
-                    if teste == "sim" or teste == "s":
-                        continue
-                    else:
-                        break
-                elif taxas_escolhidas == 3:
-                    print("----------------------------------------------------")
-                    print(f"Desconto de 10% sobre o Foro, Laudêmio e Dominio: R$ {((valor_foro * quantidade_foros) + valor_laudemio + valor_dominio) - (((valor_foro * quantidade_foros) + valor_laudemio + valor_dominio) * 0.1):.2f} \nDesconto de 8% sobre o Foro e Laudêmio, com parcelamento de 3 vezes sobre Domínio: R$ {((valor_foro * quantidade_foros) + valor_laudemio) - (((valor_foro * quantidade_foros) + valor_laudemio) * 0.08):.2f} + 3x R$ {valor_dominio / 3:.2f}")
-                    print("----------------------------------------------------")
-                    teste = input("Gostaria de calcular outro desconto? (s/n): ").strip().lower()
-                    if teste == "sim" or teste == "s":
-                        continue
-                    else:
-                        break
-                elif taxas_escolhidas == 4:
-                    print("----------------------------------------------------")
-                    print(f"Desconto de 8% sobre o Foro e Dominio: R$ {((valor_foro * quantidade_foros) + valor_dominio) - (((valor_foro * quantidade_foros) + valor_dominio) * 0.08):.2f}")
-                    print(f"Desconto de 10% sobre o Foro e Dominio: R$ {((valor_foro * quantidade_foros) + valor_dominio) - (((valor_foro * quantidade_foros) + valor_dominio) * 0.10):.2f}")
-                    print("----------------------------------------------------")
-                    teste = input("Gostaria de calcular outro desconto? (s/n): ").strip().lower()
-                    if teste == "sim" or teste == "s":
-                        continue
-                    else:
-                        break
-            elif tipo_desconto == 2:
+            tipo_desconto = input("")
+            if tipo_desconto == "sair":
+                break
+            if tipo_desconto == "1":
+                while True:
+                    print("Escolha as taxas para desconto: \n1 - Apenas foro \n2 - Foro e Laudêmio \n3 - Foro, Laudemio e Dominio \n4 - Foro e Dominio")
+                    taxas_escolhidas = int(input(""))
+                    if taxas_escolhidas == 1:
+                        print("----------------------------------------------------")
+                        print(f"Desconto de 5% sobre o Foro: R$ {(valor_foro * quantidade_foros) - ((valor_foro * quantidade_foros) * 0.05):.2f}")
+                        print("----------------------------------------------------")
+                        teste = input("Gostaria de calcular outro desconto? (s/n): ").strip().lower()
+                        if teste == "sim" or teste == "s":
+                            continue
+                        else:
+                            break
+                    elif taxas_escolhidas == 2:
+                        print("----------------------------------------------------")
+                        print(f"Desconto de 8% sobre o Foro e Laudêmio: R$ {((valor_foro * quantidade_foros) + valor_laudemio) - (((valor_foro * quantidade_foros) + valor_laudemio) * 0.08):.2f} \nDesconto de 10% sobre o Foro e Laudêmio: R$ {((valor_foro * quantidade_foros) + valor_laudemio) - (((valor_foro * quantidade_foros) + valor_laudemio) * 0.10):.2f}")
+                        print("----------------------------------------------------")
+                        teste = input("Gostaria de calcular outro desconto? (s/n): ").strip().lower()
+                        if teste == "sim" or teste == "s":
+                            continue
+                        else:
+                            break
+                    elif taxas_escolhidas == 3:
+                        print("----------------------------------------------------")
+                        print(f"Desconto de 10% sobre o Foro, Laudêmio e Dominio: R$ {((valor_foro * quantidade_foros) + valor_laudemio + valor_dominio) - (((valor_foro * quantidade_foros) + valor_laudemio + valor_dominio) * 0.1):.2f} \nDesconto de 8% sobre o Foro e Laudêmio, com parcelamento de 3 vezes sobre Domínio: R$ {((valor_foro * quantidade_foros) + valor_laudemio) - (((valor_foro * quantidade_foros) + valor_laudemio) * 0.08):.2f} + 3x R$ {valor_dominio / 3:.2f}")
+                        print("----------------------------------------------------")
+                        teste = input("Gostaria de calcular outro desconto? (s/n): ").strip().lower()
+                        if teste == "sim" or teste == "s":
+                            continue
+                        else:
+                            break
+                    elif taxas_escolhidas == 4:
+                        print("----------------------------------------------------")
+                        print(f"Desconto de 8% sobre o Foro e Dominio: R$ {((valor_foro * quantidade_foros) + valor_dominio) - (((valor_foro * quantidade_foros) + valor_dominio) * 0.08):.2f}")
+                        print(f"Desconto de 10% sobre o Foro e Dominio: R$ {((valor_foro * quantidade_foros) + valor_dominio) - (((valor_foro * quantidade_foros) + valor_dominio) * 0.10):.2f}")
+                        print("----------------------------------------------------")
+                        teste = input("Gostaria de calcular outro desconto? (s/n): ").strip().lower()
+                        if teste == "sim" or teste == "s":
+                            continue
+                        else:
+                            break
+            elif tipo_desconto == "2":
                 desconto_personalizado = float(input("Digite o porcentagem do desconto personalizado: "))
                 print("----------------------------------------------------")
                 print(f"Valor Foro com desconto personalizado: R$ {funcoes.funcoes_calculos.calculo_desconto_personalizado(valor_foro * quantidade_foros, desconto_personalizado) :.2f} \nValor Laudêmio com desconto personalizado: R$ {funcoes.funcoes_calculos.calculo_desconto_personalizado(valor_laudemio, desconto_personalizado) :.2f} \nValor Domínio Direto com desconto personalizado: R$ {funcoes.funcoes_calculos.calculo_desconto_personalizado(valor_dominio, desconto_personalizado) :.2f} \nValor Total com desconto personalizado: R$ {funcoes.funcoes_calculos.calculo_desconto_personalizado((valor_foro * quantidade_foros) + valor_laudemio + valor_dominio, desconto_personalizado) :.2f}")
